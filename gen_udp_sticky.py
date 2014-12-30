9c3f4d90bf90ffe7bfb8c911e332242d06193270
ajnouri <ajn.bin@gmail.com>
2014-12-30 23:44:46 +0100

    1st push

---
+++ gen_udp_sticky.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import random
+from scapy.all import *
+import time
+import Queue
+
+
+dsthost = '66.66.66.66'
+
+q = Queue.Queue(maxsize=5)
+
+for i in xrange(1000):
+    rint = random.randint(1,10)
+    if rint % 5 == 0:
+        print '==> Random queue processing'
+        if not q.full():
+            ipin = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+            q.put(ipin)
+            srchost = ipin
+            print ipin,' into the queue'
+        else:
+            ipout = q.get()
+            srchost = ipout
+            print ' *** This is sticky src IP',ipout
+    else:
+        srchost = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+        print 'one time src IP', srchost
+    #srchost = scapy.RandIP()
+    p = IP(src=srchost,dst=dsthost) / UDP(dport=5555)
+    print 'src= ',srchost, 'dst= ',dsthost
+    send(p, iface='tap2')
+    print 'sending packet\n'
+    time.sleep(1)
